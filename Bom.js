978d113afe6a607f7987976ca059ae5bd9c6e50b
Islam Atwa <Islam Atwa>
2024-10-31 16:30:49 +0300

    initial commit

---
+++ Bom.js
@@ -0,0 +1,46 @@
+/*
+JavaScript BOM
+    The Browser Object Model (BOM) is the core of JavaScript on the web.
+    The BOM provides you with objects that expose the web browser’s functionality.
+*/
+
+/*
+There are 4 main  section 
+    1- Section Window 
+    2- Section loction
+    3- Section navigator
+    4- Section screen   
+    5- Section history
+*/
+
+/*
+Section 1. Window
+    Window – understand the window object.
+    Alert – display an alert dialog.
+    Confirm – display a modal dialog with a question.
+    Prompt – prompt the user to input some text.
+    setTimeout – set a timer and execute a callback function once the timer expires.
+    setInterval – execute a callback function repeatedly with a fixed delay between each call.
+*/
+
+/*
+Section 2. Location
+    Location – manipulate the location of a document via the location object.
+    Get query string parameters – learn how to retrieve query string parameters.
+    Redirect to a new URL – show you how to redirect to a new URL using JavaScript.
+*/
+
+/*
+Section 3. Navigator
+    Navigator – query the information and capabilities of the browser using the navigator object.
+*/
+
+/*
+Section 4. Screen
+    Screen – get the information about the screen on which the browser is running by using the screen object.   
+*/
+
+/*
+Section 5. History
+    History – manage the web browser’s history stack with the history object.
+*/
